7ce30277581033321e9b560290dd725bd1edab70
Gita Rahmi <gitarahmifajrin01@gmail.com>
2022-04-19 15:51:04 +0700

    Add project files.

---
+++ ceksaldo/Bank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ceksaldo
+{
+	public class Bank
+	{
+		public class BankAccount : Bank
+		{
+			List<double> DepositList = new List<double>();
+			//object[] DepositArr = new object[6];
+			public string username { get; set; }
+
+			public double Balance { get; set; }
+
+			public BankAccount(string username, double Balance)
+			{
+				this.username = username;
+				this.Balance = Balance;
+
+				Console.WriteLine("Username: {0} , Balance: {1}", username, Balance);
+			}
+		}
+	}
+}
---
+++ ceksaldo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ceksaldo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bank bank = new Bank();
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
